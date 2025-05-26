9385c59b2c6c1dccb4fd04e2bf31022a1e1d5c40
鸳汐 <39462133+DreamedWorker@users.noreply.github.com>
2025-05-26 15:47:43 +0800

    feat(OTA): 应用内更新

--- Asagl/AsaglApp.swift
+++ Asagl/AsaglApp.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import Sparkle
 
 @main
 struct AsaglApp: App {
     @NSApplicationDelegateAdaptor(AsaglAppDelegate.self) private var delegate
     @StateObject private var svm = SharedViewModel.shared
+    private let updaterController: SPUStandardUpdaterController
+    
+    init() {
+        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
+        // This is where you can also pass an updater delegate if you need one
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
     
     var body: some Scene {
         WindowGroup {
@@ -24,6 +32,9 @@ struct AsaglApp: App {
         .windowResizability(.contentSize)
         .commands(content: {
             CommandGroup(replacing: .newItem, addition: {})
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
         })
     }
 }
